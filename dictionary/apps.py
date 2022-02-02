17186a717454e7279079d30486cbc708a409c1d7
Ata <atakanozalan@gmail.com>
2022-02-02 10:11:30 +0300

    '2022'

--- dictionary/apps.py
+++ dictionary/apps.py
@@ -204,7 +204,7 @@ class DictionaryConfig(AppConfig):
     You may also (better) use this for debugging purposes.
     """
 
-    YEAR_RANGE = (2020, 2019, 2018)
+    YEAR_RANGE = (2022)
     """Years available for today-in-history"""
 
     #  <-----> END OF CATEGORY RELATED SETTINGS <----->  #
